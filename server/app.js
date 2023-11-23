7425247cebf625c4501f758b406013a826bf7833
yichenlai16 <63837651+yichenlai16@users.noreply.github.com>
2023-11-24 01:01:56 +0800

    feat: add /user POST method

--- server/app.js
+++ server/app.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 3000;
-
 const expressWs = require("express-ws")(app);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 const Router = require("./router");
 
 app.use(Router);
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error(error));
 
 app.listen(port, () => {
   console.log(`Server app listening on port ${port}`);
--- server/router.js
+++ server/router.js
@@ -2,7 +2,25 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Home");
+  res.json({ message: "root endpoint" });
+});
+
+// User
+const User = require("./model/user");
+
+// router.get("/api/users", async (ws, req) => {
+//   const allUsers = await User.findALL();
+//   return res.status(200).json(allUsers);
+// });
+
+router.post("/api/users", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.send(user);
+  } catch (e) {
+    res.status(404).send(e);
+  }
 });
 
 router.ws("/ws", function (ws, req) {
